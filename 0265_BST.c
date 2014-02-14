6f2df1952ce72fec64bae6ecfbb6d99f3d1c563c
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-02-14 21:32:20 +0530

    okay, so i'm not completely clueless for the lab exam

--- 0265_BST.c
+++ 0265_BST.c
@@ -1,11 +1,51 @@
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct BST
+typedef struct node
 {
 	int data;
-	node *leftChild,*rightChild;
-}BST;
+	struct node *leftChild,*rightChild;
+}node;
+void insert(node *tree,node *leaf)
+{
+	if(tree==NULL)					//base case for the recursion
+		tree=leaf;
+	else
+	{
+		if(tree->data<leaf->data)
+			insert(tree->leftChild,leaf);
+		else
+			insert(tree->rightChild,leaf);
+	}
+}
 int main()
 {
+	node *root;
+	int n,choice;
+	printf("Input number of nodes\n");
+	scanf("%d",&n);
+	printf("Insert into BST\n");
+	root=NULL;
+	while(n--)
+	{
+		node *temp;
+		temp=(node *)malloc(sizeof(node));
+		scanf("%d",&temp->data);
+		temp->leftChild=temp->rightChild=NULL;
+		if(root==NULL)
+			root=temp;
+		else
+			insert(root,temp);
+	}
+	printf("Enter search value\n");
+	scanf("%d",&n);
+	search(root,n);
+	printf("Enter value to delete\n");
+	delete(root,n);
+	printf("In-order printing\n");
+	inOrder(root);
+	printf("Pre-order printing\n");
+	preOrder(root);
+	printf("Post-order printing\n");
+	postOrder(root);
 	return 0;
 }
