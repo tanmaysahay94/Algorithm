6424562e1a69eb3189a2efd6bed292feab7ca3cc
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-03-30 20:22:19 +0530

    phew

--- 0289_online.c
+++ 0289_online.c
@@ -1,57 +1,109 @@
 #include<stdio.h>
-#include<stdlib.h>
-void shuffleUp(int a[],int size,int i)
+int high[1000000],low[1000000],highCount,lowCount;
+void insertMin(int val)
 {
-	int left=2*i,right=2*i+1,minIndex=i;
-	if(left<size&&a[left]<a[minIndex])
-		minIndex=left;
-	if(right<size&&a[right]<a[minIndex])
-		minIndex=right;
-	if(minIndex!=i)
+	highCount++;
+	high[highCount]=val;
+	int now=highCount;
+	while(high[now/2]>val)
 	{
-		a[i]=a[i]^a[minIndex]^(a[minIndex]=a[i]);
-		shuffleUp(a,size,minIndex);
+		high[now]=high[now/2];
+		now/=2;
 	}
+	high[now]=val;
 }
-void generateMinHeap(int a[],int size)
+void insertMax(int val)
 {
-	int i;
-	for(i=size/2;i>=0;i--)
-		shuffleUp(a,size,i);
-}
-int kthLargest(int a[],int size,int k)
-{
-	int i,minHeap[k];
-	for(i=0;i<k;i++)
-		minHeap[i]=a[i];
-	generateMinHeap(minHeap,k);
-	for(i=k;i<size;i++)
+	lowCount++;
+	low[lowCount]=val;
+	int now=lowCount;
+	while(low[now/2]<val)
 	{
-		if(a[i]>minHeap[0])
+		low[now]=low[now/2];
+		now/=2;
+	}
+	low[now]=val;
+}
+int deleteMin()
+{
+		int minElement,lastElement,child,now;
+		minElement=high[1];
+		lastElement=high[highCount--];
+		for(now=1;now*2<=highCount;now=child)
 		{
-			minHeap[0]=a[i];
-			shuffleUp(minHeap,k,0);
+			child=now*2;
+			if(child!=highCount&&high[child+1]<high[child])
+				child++;
+			if(lastElement>high[child])
+				high[now]=high[child];
+			else
+				break;
 		}
+		high[now]=lastElement;
+		return minElement;
+}
+int deleteMax()
+{
+	int maxElement,lastElement,child,now;
+	maxElement=low[1];
+	lastElement=low[lowCount--];
+	for(now=1;now*2<=lowCount;now=child)
+	{
+		child=now*2;
+		if(child!=lowCount&&low[child+1]>low[child])
+			child++;
+		if(lastElement<low[child])
+			low[now]=low[child];
+		else
+			break;
 	}
-	return minHeap[0];
+	low[now]=lastElement;
+	return maxElement;
 }
 int main()
 {
-	int a[500000],testTakers=0,qualified,numberOfQueries,query,input;
-	scanf("%d",&numberOfQueries);
-	while(numberOfQueries--)
+	highCount=lowCount=0;
+	high[0]=-2147483648;
+	low[0]=2147483647;
+	int n,query,num,total=0;
+	scanf("%d",&n);
+	while(n--)
 	{
 		scanf("%d",&query);
 		if(query==1)
 		{
-			qualified=testTakers/4;
-			if(!qualified)
-				printf("-1\n");
+			if(total/4)
+				printf("%d\n",high[1]);
 			else
-				printf("%d\n",kthLargest(a,testTakers,qualified));
+				printf("-1\n");
 		}
 		else if(query==2)
-			scanf("%d",&a[testTakers++]);
+		{
+			scanf("%d",&num);
+			total++;
+			if(total<=3)
+				insertMax(num);
+			else if(total%4==0)
+			{
+				if(num>=low[1])
+					insertMin(num);
+				else
+				{
+					insertMin(deleteMax());
+					insertMax(num);
+				}
+			}
+			else
+			{
+				if(num>=high[1])
+				{
+					insertMax(deleteMin());
+					insertMin(num);
+				}
+				else
+					insertMax(num);
+			}
+		}
 	}
 	return 0;
 }
--- experiment.c
+++ experiment.c
@@ -2,13 +2,10 @@
 
 int main()
 {
-#include < stdio.h >
-	int main(){
-		    char char_array[]="Hello\0World";
-		        char *str="Hello\0World";
-			    printf("%s %c\n",char_array,char_array[1]);
-			        printf("%s %c\n",str,str[1]);
-				    printf("%d %d\n",sizeof(char_array),sizeof(str));
-				        return 0;
-	}
+	char char_array[]="Hello\0World";
+	char *str="Hello\0World";
+	printf("%s %c\n",char_array,char_array[1]);
+	printf("%s %c\n",str,str[1]);
+	printf("%d %d\n",sizeof(char_array),sizeof(str));
+	return 0;
 }
