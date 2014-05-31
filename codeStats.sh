997a0da30e0a08f24eae4f1aab5521c0a28fa312
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-05-31 10:51:53 +0530

    topcoder badneighbours done. phew.

--- codeStats.sh
+++ codeStats.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo
-a=`cat *c | wc -l`
-b=`cat *py | wc -l`
-c=`expr " $a + $b " | bc -l`
-d=`ls *.c | wc -l`
+a=`cat *.c* | wc -l`
+b=`cat *.py | wc -l`
+c=`expr " $a + $b" | bc -l`
+d=`ls *.c* | wc -l`
 e=`ls *.py | wc -l`
-f=`expr " $d + $e " | bc -l`
+f=`expr " $d + $e" | bc -l`
 g=`expr " $c / $f " | bc -l`
 echo "Total Lines of Code: $c"
-echo "C programs: $d"
+echo "C and CPP programs: $d"
 echo "Python programs: $e"
 echo "Total Number of programs: $f"
 echo -n 'Lines of Code per program: '
