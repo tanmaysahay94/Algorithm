dc8e6e835f869575c25f885279d6194fc478e9a0
Tanmay Sahay <tanmaysahay94@gmail.com>
2014-06-02 15:23:51 +0530

    Create 0330_toy.cpp

---
+++ 0330_toy.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+int main()
+{
+    int i, n, m, a, b, ans = 0;
+    cin >> n >> m;
+    int v[n];
+    for (i = 0; i < n; i++)
+        cin >> v[i];
+    for (i = 0; i < m; i++)
+    {
+        cin >> a >> b;
+        a--;
+        b--;
+        ans += min(v[a], v[b]);
+    }
+    cout << ans << endl;
+}
