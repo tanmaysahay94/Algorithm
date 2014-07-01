b7010af3246362fc297089d0f8dac4287f1e2b4b
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-07-01 20:08:39 +0530

    good codeforces today

--- 0374_runningStudent.cpp
+++ 0374_runningStudent.cpp
@@ -15,10 +15,27 @@
 #include <stack>
 #include <cstdlib>
 #include <set>
+
 using namespace std;
+
 int main()
 {
-	double x, y;
-	cin >> x >> y;
-	cout << atan2(y, x) * 180 / M_PI << endl;
+	double vb, vs, ux, uy, curr, min = 1e14;
+	int i, n, idx;
+	cin >> n >> vb >> vs;
+	vector<double> loc(n);
+	for (i = 0; i < n; i++)
+		cin >> loc[i];
+	cin >> ux >> uy;
+	for (i = 1; i < n; i++)
+	{
+		curr = loc[i]/vb + pow(pow(ux - loc[i], 2) + pow(uy, 2), 0.5)/vs;
+		if (curr <= min)
+		{
+			min = curr;
+			idx = i + 1;
+		}
+	}
+	cout << idx << endl;
+	return 0;
 }
