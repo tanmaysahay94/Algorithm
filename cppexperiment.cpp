c086d13b58dd778f99948b7d38bd02a7b6431a39
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-07-02 01:28:47 +0530

    uneventful part of the day

--- cppexperiment.cpp
+++ cppexperiment.cpp
@@ -16,9 +16,30 @@
 #include <cstdlib>
 #include <set>
 using namespace std;
+int dot(int ax, int ay, int bx, int by, int cx, int cy)
+{
+	return (ax - bx) * (bx - cx) + (ay - by) * (by - cy);
+}
+int c(int ax, int ay, int bx, int by)
+{
+	if (ax == bx and ay == by)
+		return 0;
+	return 1;
+}
+int t(int ax, int ay, int bx, int by, int cx, int cy)
+{
+	if (c(ax, ay, bx, by) and c(ax, ay, cx, cy) and c(bx, by, cx, cy))
+		return 1;
+	return 0;
+}
 int main()
 {
-	double x, y;
-	cin >> x >> y;
-	cout << atan2(y, x) * 180 / M_PI << endl;
+	int te;
+	cin >> te;
+	while (te--)
+	{
+		int ax, ay, bx, by, cx, cy;
+		cin >> ax >> ay >> bx >> by >> cx >> cy;
+		printf("dot: %d triangle: %d\n", dot(ax, ay, bx, by, cx, cy), t(ax, ay, bx, by, cx, cy));
+	}
 }
