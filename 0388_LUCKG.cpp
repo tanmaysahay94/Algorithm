e110bd1d025aed79c2cf118f9811f1094b5eba5c
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-07-15 14:29:24 +0530

    syncing

--- 0388_LUCKG.cpp
+++ 0388_LUCKG.cpp
@@ -23,14 +23,42 @@ typedef unsigned long long ULL;
 int main()
 {
 	char err[] = "No Solution";
-	ULL T, n, m, i;
+	ULL T, n, m, i, idx;
 	scanf("%llu", &T);
 	while (T--)
 	{
 		scanf("%llu%llu", &n, &m);
 		vector<ULL> p(n);
+		vector<vector<ULL> > cycle;
+		vector<bool> visited(n, false);
 		for (i = 0; i < n; i++)
 			scanf("%llu", p[i]);
+		if (n <= 2 or m <= 2)
+		{
+			printf("No Solution\n");
+			continue;
+		}
+		for (i = 0; i < n; i++)
+			if (not visited[i])
+			{
+				vector<ULL> curr;
+				curr.push_back(i);
+				idx = p[i];
+				while (idx != i)
+				{
+					curr.push_back(idx);
+					idx = p[idx];
+				}
+				cycle.push_back(curr);
+			}
+		vector<ULL> s(cycle.length());
+		ULL sum = cycle[0].length;
+		s[0] = sum * m;
+		for (i = 1; i < cycle.length(); i++)
+		{
+			sum += cycle[i].length();
+			s[i] = sum * m;
+		}
 	}
 	return 0;
 }
