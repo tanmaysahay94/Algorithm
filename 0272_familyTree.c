d53963cefaf04f350dfb0a2f7b42d4a854747487
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-07-16 13:13:49 +0530

    practising codeforces round #FF

--- 0272_familyTree.c
+++ 0272_familyTree.c
@@ -1,2 +1,20 @@
 #include<stdio.h>
-int main(){long long int t,n,a,b;scanf("%lld",&t);while(t--){scanf("%lld%lld%lld",&n,&a,&b);while(a!=b){if(a>b)a>>=1;else b>>=1;}printf("%lld\n",a);}return 0;}
+int
+main ()
+{
+  long long int t, n, a, b;
+  scanf ("%lld", &t);
+  while (t--)
+    {
+      scanf ("%lld%lld%lld", &n, &a, &b);
+      while (a != b)
+	{
+	  if (a > b)
+	    a >>= 1;
+	  else
+	    b >>= 1;
+	}
+      printf ("%lld\n", a);
+    }
+  return 0;
+}
--- 0273_bstOp.c
+++ 0273_bstOp.c
@@ -2,114 +2,125 @@
 #include<stdlib.h>
 typedef struct node
 {
-	int data;
-	struct node *left,*right;
-}node;
-int lookUpHeight,foundMyNode,foundIRNode,subTreeCount,subTreeInOrder[1000000],levelElementCount,level[1000000];
-void insert(node *tree,node *leaf)
+  int data;
+  struct node *left, *right;
+} node;
+int lookUpHeight, foundMyNode, foundIRNode, subTreeCount,
+  subTreeInOrder[1000000], levelElementCount, level[1000000];
+void
+insert (node * tree, node * leaf)
 {
-	if(leaf->data<tree->data)
-		if(tree->left)
-			insert(tree->left,leaf);
-		else
-			tree->left=leaf;
+  if (leaf->data < tree->data)
+    if (tree->left)
+      insert (tree->left, leaf);
+    else
+      tree->left = leaf;
+  else if (tree->right)
+    insert (tree->right, leaf);
+  else
+    tree->right = leaf;
+}
+
+int
+getHeight (node * tree, int val)
+{
+  if (tree)
+    if (val == tree->data)
+      return lookUpHeight;
+    else
+      {
+	lookUpHeight++;
+	if (val < tree->data)
+	  getHeight (tree->left, val);
 	else
-		if(tree->right)
-			insert(tree->right,leaf);
-		else
-			tree->right=leaf;
+	  getHeight (tree->right, val);
+      }
+  else
+    return 0;
 }
-int getHeight(node *tree,int val)
+
+void
+generateSubTree (node * tree)
 {
-	if(tree)
-		if(val==tree->data)
-			return lookUpHeight;
-		else
-		{
-			lookUpHeight++;
-			if(val<tree->data)
-				getHeight(tree->left,val);
-			else
-				getHeight(tree->right,val);
-		}
-	else
-		return 0;
+  if (tree)
+    {
+      generateSubTree (tree->left);
+      subTreeInOrder[subTreeCount++] = tree->data;
+      generateSubTree (tree->right);
+    }
 }
-void generateSubTree(node *tree)
+
+void
+levelCreate (node * tree, int height)
 {
-	if(tree)
+  if (tree)
+    {
+      if (height == 1)
+	level[levelElementCount++] = tree->data;
+      else
 	{
-		generateSubTree(tree->left);
-		subTreeInOrder[subTreeCount++]=tree->data;
-		generateSubTree(tree->right);
+	  levelCreate (tree->left, height - 1);
+	  levelCreate (tree->right, height - 1);
 	}
+    }
 }
-void levelCreate(node *tree,int height)
+
+void
+findAndGenerate (node * tree, int val)
 {
-	if(tree)
-	{
-		if(height==1)
-			level[levelElementCount++]=tree->data;
-		else
-		{
-			levelCreate(tree->left,height-1);
-			levelCreate(tree->right,height-1);
-		}
-	}
+  if (tree)
+    {
+      if (val < tree->data)
+	findAndGenerate (tree->left, val);
+      else if (val > tree->data)
+	findAndGenerate (tree->right, val);
+      else
+	generateSubTree (tree);
+    }
 }
-void findAndGenerate(node *tree,int val)
+
+int
+main ()
 {
-	if(tree)
+  int t, n, myNode, k, i;
+  scanf ("%d", &t);
+  while (t--)
+    {
+      levelElementCount = foundMyNode = foundIRNode = subTreeCount = 0;
+      lookUpHeight = 1;
+      node *bst = NULL, *temp;
+      scanf ("%d", &n);
+      while (n--)
 	{
-		if(val<tree->data)
-			findAndGenerate(tree->left,val);
-		else if(val>tree->data)
-			findAndGenerate(tree->right,val);
-		else 
-			generateSubTree(tree);
+	  temp = (node *) malloc (sizeof (node));
+	  scanf ("%d", &temp->data);
+	  temp->left = temp->right = NULL;
+	  if (bst == NULL)
+	    bst = temp;
+	  else
+	    insert (bst, temp);
 	}
-}
-int main()
-{
-	int t,n,myNode,k,i;
-	scanf("%d",&t);
-	while(t--)
+      scanf ("%d%d", &myNode, &k);
+      if (lookUpHeight = getHeight (bst, myNode))
 	{
-		levelElementCount=foundMyNode=foundIRNode=subTreeCount=0;
-		lookUpHeight=1;
-		node *bst=NULL,*temp;
-		scanf("%d",&n);
-		while(n--)
-		{
-			temp=(node *)malloc(sizeof(node));
-			scanf("%d",&temp->data);
-			temp->left=temp->right=NULL;
-			if(bst==NULL)
-				bst=temp;
-			else
-				insert(bst,temp);
-		}
-		scanf("%d%d",&myNode,&k);
-		if(lookUpHeight=getHeight(bst,myNode))
-		{
-			levelCreate(bst,lookUpHeight);
-			for(i=0;i<levelElementCount;i++)
-				if(level[i]==myNode)
-					break;
-			if(i==levelElementCount-1)
-				printf("$ $\n");
-			else
-			{
-				printf("%d ",level[i+1]);
-				findAndGenerate(bst,level[i+1]);
-				if(k>subTreeCount)
-					printf("$\n");
-				else
-					printf("%d\n",subTreeInOrder[k-1]);
-			}
-		}
-		else
-			printf("$ $\n");
+	  levelCreate (bst, lookUpHeight);
+	  for (i = 0; i < levelElementCount; i++)
+	    if (level[i] == myNode)
+	      break;
+	  if (i == levelElementCount - 1)
+	    printf ("$ $\n");
+	  else
+	    {
+	      printf ("%d ", level[i + 1]);
+	      findAndGenerate (bst, level[i + 1]);
+	      if (k > subTreeCount)
+		printf ("$\n");
+	      else
+		printf ("%d\n", subTreeInOrder[k - 1]);
+	    }
 	}
-	return 0;
+      else
+	printf ("$ $\n");
+    }
+  return 0;
 }
