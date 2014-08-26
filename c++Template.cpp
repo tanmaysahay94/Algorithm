d13146c9468ec06a2a569898b5b226e31d4bbd30
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-08-26 21:36:23 +0530

    removed retardedness

--- c++Template.cpp
+++ c++Template.cpp
@@ -1,21 +1,5 @@
 :insert
-#include <iostream>
-#include <vector>
-#include <cstdio>
-#include <cstring>
-#define _USE_MATH_DEFINES
-#include <cmath>
-#include <map>
-#include <queue>
-#include <deque>
-#include <functional>
-#include <algorithm>
-#include <cassert>
-#include <utility>
 #include <bits/stdc++.h>
-#include <stack>
-#include <cstdlib>
-#include <set>
 
 using namespace std;
 
