0c2cb3e4091bc15f78876e0de433beb1f6d94628
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-08-26 21:28:28 +0530

    c++ template

--- c++Template.cpp
+++ c++Template.cpp
@@ -19,11 +19,18 @@
 
 using namespace std;
 
+typedef long long LL;
 typedef unsigned long long ULL;
+typedef vector<int> VI;
 
 #define SI(n) scanf("%d", &n)
+#define SLL(n) scanf("%lld", &n)
 #define SULL(n) scanf("%llu", &n)
 #define sortv(v) sort(v.begin(), v.end())
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+#define f first
+#define s second
 
 int main()
 {
