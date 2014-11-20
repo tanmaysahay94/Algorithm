b88247fe703851a29095692ba898eddc4ac9cecd
tanmaysahay94 <tanmaysahay94@gmail.com>
2014-11-20 17:43:24 +0530

    class prepared

--- 0510_fenwickTreeRange.cpp
+++ 0510_fenwickTreeRange.cpp
@@ -18,66 +18,61 @@ typedef vector<LL> VLL;
 #define f first
 #define s second
 
-LL n;
-VLL bit1, bit2;
-
-void update(VLL& bit, LL idx, LL val)
-{
-	for (; idx <= n; idx += LSOne(idx))
-		bit[idx] += val;
-}
-
-void rangeUpdate(LL p, LL q, LL val)
-{
-	update(bit1, p, val);
-	update(bit1, q + 1, -val);
-	update(bit2, p, val * (p - 1));
-	update(bit2, q + 1, -val * q);
-}
-
-LL query(VLL& bit, LL idx)
-{
-	LL ans = 0;
-	for (; idx; idx -= LSOne(idx))
-		ans += bit[idx];
-	return ans;
-}
-
-LL query(LL idx)
-{
-	return idx * query(bit1, idx) - query(bit2, idx);
-}
-
-LL rangeQuery(LL p, LL q)
-{
-	return query(q) - query(p - 1);
-}
-
-void clean(VLL& bit, LL size)
-{
-	bit.resize(size);
-	for (LL i = 0; i < size; i++)
-		bit[i] = 0;
-}
+class fenwickTreeRange {
+	LL n;
+	VLL bit1, bit2;
+	public:
+	void clean(VLL& bit, LL size) {
+		bit.resize(size);
+		for (LL i = 0; i < size; i++)
+			bit[i] = 0;
+	}
+	void update(VLL& bit, LL idx, LL val) {
+		for (; idx <= n; idx += LSOne(idx))
+			bit[idx] += val;
+	}
+	void rangeUpdate(LL p, LL q, LL val) {
+		update(bit1, p, val);
+		update(bit1, q + 1, -val);
+		update(bit2, p, val * (p - 1));
+		update(bit2, q + 1, -val * q);
+	}
+	LL query(VLL& bit, LL idx) {
+		LL ans = 0;
+		for (; idx; idx -= LSOne(idx))
+			ans += bit[idx];
+		return ans;
+	}
+	LL query(LL idx) {
+		return idx * query(bit1, idx) - query(bit2, idx);
+	}
+	LL rangeQuery(LL p, LL q) {
+		return query(q) - query(p - 1);
+	}
+	fenwickTreeRange(LL N): n(N) {
+		clean(bit1, n + 1);
+		clean(bit2, n + 1);
+	}
+};
 
 int main()
 {
-	LL t, c, type, p, q, v;
+	LL n, t, c, type, p, q, v;
 	SLL(t);
 	while (t--)
 	{
 		SLL(n); SLL(c);
-		clean(bit1, n + 1); clean(bit2, n + 1);
+		fenwickTreeRange f(n);
 		while (c--)
 		{
 			SLL(type); SLL(p); SLL(q);
 			if (!type)
 			{
 				SLL(v);
-				rangeUpdate(p, q, v);
+				f.rangeUpdate(p, q, v);
 			}
 			else
-				printf("%lld\n", rangeQuery(p, q));
+				printf("%lld\n", f.rangeQuery(p, q));
 		}
 	}
 	return 0;
