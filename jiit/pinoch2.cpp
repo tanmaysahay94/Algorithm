38cba08d9e19922d4ab18ade2b0cede362da4606
tanmaysahay94 <tanmaysahay94@gmail.com>
2015-05-15 00:06:51 +0530

    meh, could have done way better...

---
+++ jiit/pinoch2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void optimizeIO()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+}
+
+vector<long long> v;
+
+int main()
+{
+	optimizeIO();
+	long long t, n;
+	cin >> t;
+	while (t--)
+	{
+		cin >> n;
+		v = vector<long long> (n);
+		for (int i = 0; i < n; i++)
+			cin >> v[i];
+		int ans = 0, tmp = 0;
+		for (int i = 1; i < n; i++)
+			if (v[i] == v[i - 1])
+				tmp++;
+			else
+			{
+				ans = max(ans, tmp);
+				tmp = 0;
+			}
+		ans = max(ans, tmp);
+		cout << ans << endl;
+	}
+	return 0;
+}
