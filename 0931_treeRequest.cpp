bb09e15c7e7660bb3c15c76621a3f7b9bac00dcb
tanmaysahay94 <tanmaysahay94@gmail.com>
2015-08-20 04:31:16 +0530

    I love this code <3

--- 0931_treeRequest.cpp
+++ 0931_treeRequest.cpp
@@ -2,40 +2,76 @@
 
 using namespace std;
 
-const int maxn = 500005;
+const int maxn = 5e5 + 5;
+int n, m;
+vector<int> G[maxn], pre[maxn], Nlvl[maxn], mapTo(maxn), L(maxn), last(maxn);
+char S[maxn];
+int id;
 
-int n, m, id = 1;
-int H[maxn], HR[maxn], L[maxn], E[maxn];
-vector<int> G[maxn], LH[maxn];
-char s[maxn];
-
-void dfs(int v = 1, int l = 1)
+void input()
 {
-	H[v] = id;
-	HR[id] = v;
-	L[v] = l;
-	LH[l].push_back(id);
-	id++;
-	if (not G[v].size())
+	id = 0;
+	scanf("%d%d", &n, &m);
+	for (int i = 2; i <= n; i++)
 	{
-		E[v] = H[v];
-		return;
+		int parent;
+		scanf("%d", &parent);
+		G[parent].push_back(i);
 	}
-	for (auto u: G[v])
-		dfs(u, l + 1);
-	E[v] = id - 1;
+	scanf("%s", S);
+}
+
+void dfs(int v = 1, int depth = 1)
+{
+	mapTo[v] = ++id;
+	L[v] = depth;
+	Nlvl[depth].push_back(id);
+	pre[depth].push_back(1 << (S[v - 1] - 'a'));
+	for (auto g: G[v])
+		dfs(g, depth + 1);
+	last[v] = id;
+}
+
+void genPre()
+{
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j < (int) pre[i].size(); j++)
+			pre[i][j] ^= pre[i][j - 1];
+}
+
+void solve()
+{
+	int v, h;
+	while (m--)
+	{
+		scanf("%d%d", &v, &h);
+		int start = mapTo[v];
+		int end = last[v];
+		vector<int>::iterator sit = lower_bound(Nlvl[h].begin(), Nlvl[h].end(), start);
+		vector<int>::iterator eit = upper_bound(Nlvl[h].begin(), Nlvl[h].end(), end);
+		if(sit == eit)
+		{
+			printf("Yes\n");
+			continue;
+		}
+		eit--;
+		int sidx = sit - Nlvl[h].begin();
+		int eidx = eit - Nlvl[h].begin();
+		int cnt = pre[h][eidx];
+		if (sidx)
+			cnt ^= pre[h][--sidx];
+		if (! (cnt & (cnt - 1)))
+			printf("Yes\n");
+		else
+			printf("No\n");
+	}
 }
 
 int main()
 {
-	scanf("%d%d", &n, &m);
-	for (int i = 2; i <= n; i++)
-	{
-		int p;
-		scanf("%d", &p);
-		G[p].push_back(i);
-	}
+	input();
 	dfs();
-	scanf("%s", s);
+	genPre();
+	solve();
 	return 0;
 }
